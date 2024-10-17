f443ed7d13b21dc43d12ecf1a50c53d6aa9fcfd1
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2024-10-16 21:31:24 -0400

    initial commit

---
+++ knightMoves6/moves.py
@@ -0,0 +1,17 @@
+
+# layout of chess board
+
+gridLayout = [
+    ['A' , 'B' , 'B' , 'C' , 'C' , 'C'], # 6
+    ['A' , 'A' , 'B' , 'B' , 'C' , 'C'], # 5
+    ['A' , 'A' , 'B' , 'B' , 'C' , 'C'], # 4
+    ['A' , 'A' , 'B' , 'B' , 'C' , 'C'], # 3
+    ['A' , 'A' , 'A' , 'B' , 'B' , 'C'], # 2
+    ['A' , 'A' , 'A' , 'B' , 'B' , 'C'], # 1
+#     a     b     c     d     e     f
+]
+
+# two trips, 
+# one from a6 to f1 and a1 to f6
+# diagonals
+
