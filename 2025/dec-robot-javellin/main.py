b0d666c255e086ebe944bbd6c14519dcd92016a2
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2025-12-06 16:37:38 -0500

    2/8 cases done

--- 2025/dec-robot-javellin/main.py
+++ 2025/dec-robot-javellin/main.py
@@ -5,23 +5,42 @@ import polars as pl
 
 """Resolves the final Spears Robot distance. Cheater"""
 
+"""d > 1/2"""
 
-def resolveS(d: float, t1j: float, t1s: float):
 
+def resolveS_a(d: float, s1: float, bit: bool):
+    s2 = np.random.uniform(0, 1)
     # trivial cases:
 
-    bit = t1j < d
 
-    # 8 cases:
+"""d < 1/2"""
+
+
+def resolveS_b(d: float, s1: float, bit: bool):
+    s2 = np.random.uniform(0, 1)
+
+    if bit:
+        if s1 < d:
+            return s2
+        else:
+            pass
+    else:
+        if s1 < d:
+            k = 1 - s1
+            l = 2*k
+            return s2 if d < l - np.sqrt(l * (l - 1)) else s1
+        else:
+            pass
 
 
 """
-One trial given some d. d is used for checking t1j < d.
+One trial given some d. d is used for checking j1 < d.
 No need to do anything fancy for > d, uniformity and iid imply d = 1-d* at the end.
 
 """
 
-def trial(d: float, size : int = 1):
+
+def trial(d: float):
     seed = 1000 * int(np.random())
     np.random.seed(seed)
 
@@ -29,21 +48,23 @@ def trial(d: float, size : int = 1):
     tj = 0
 
     # javellin throw
-    t1j = np.random.uniform(0, 1)
-    t1s = np.random.uniform(0, 1)
+    j1 = np.random.uniform(0, 1)
+    s1 = np.random.uniform(0, 1)
 
-    tj = np.random.uniform(0, 1) if t1j < 0.5 else t1j
+    tj = np.random.uniform(0, 1) if j1 < 0.5 else j1
 
+    bit = j1 < d
     if d > 0.5:
-        ts = resolveS_a(d, t1j, t1s)
+        ts = resolveS_a(d, s1, bit)
     else:
-        ts = resolveS_b(d, t1j, t1s)
+        ts = resolveS_b(d, s1, bit)
 
-    return
+    return ts > tj
 
 
 def main():
-    "idea: Asking t1j < d should be maximally informative for Spears, thus should be "
+    "idea: Asking j1 < d should be maximally informative for Spears, thus should be "
+
 
 if __name__ == "__main__":
     main()
